bc9b161e556b6cf8c2794da23e56ce08f562c787
Ansh <thebeliever39@gmail.com>
2024-08-24 14:03:09 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -16,16 +16,16 @@ import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app=express();   
-const allowedOrigin = 'http://localhost:5173';
-app.set("trust proxy", true);
+// const allowedOrigin = 'http://localhost:5173';
+// app.set("trust proxy", true);
 
-const corsOptions = {
-  origin: allowedOrigin,
-  credentials: true, // This allows cookies and credentials to be sent in cross-origin requests
-  // optionsSuccessStatus: 200 // Some legacy browsers choke on 204
-};
+// const corsOptions = {
+//   origin: allowedOrigin,
+//   credentials: true, // This allows cookies and credentials to be sent in cross-origin requests
+//   // optionsSuccessStatus: 200 // Some legacy browsers choke on 204
+// };
 
-app.use(cors(corsOptions));
+app.use(cors("*"));
 
 app.use(morgan('dev'));
 
